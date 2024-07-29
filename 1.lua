dddf80fb26d8ed0c2e4db0df7451bb317ddf1adc
iu4w <lutendemwalwanda@gmail.com>
2024-07-29 19:33:47 +0400

    Update 1.lua

--- 1.lua
+++ 1.lua
@@ -1,7 +1,1 @@
--- 1.lua
-
-getgenv().script_key = getgenv().script_key or "default_key"
-getgenv().targetPetID = getgenv().targetPetID or 0
-
--- Load and execute 2.lua
-loadstring(game:HttpGet("https://raw.githubusercontent.com/iu4w/nemesis/main/2.lua", true))()
+rfr4
