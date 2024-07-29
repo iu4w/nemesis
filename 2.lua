be29253c83b0ca6b84563eedf599fe84f429b3ad
iu4w <lutendemwalwanda@gmail.com>
2024-07-29 19:33:40 +0400

    Update 2.lua

--- 2.lua
+++ 2.lua
@@ -1,7 +1,1 @@
--- 2.lua
-
-print("2.lua - Script Key: Checking if " .. getgenv().script_key .. " is Valid")
-print("2.lua - Target Pet ID: Checking if " .. .. getgenv().targetPetID .. " isValid")
-
--- Load and execute 3.lua
-loadstring(game:HttpGet("https://raw.githubusercontent.com/iu4w/nemesis/main/3.lua", true))()
+frvr
